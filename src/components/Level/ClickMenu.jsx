8e47a8b69dcacffdc969169df91bb32e7cc7187c
Karim Mk <mk47blast@protonmail.com>
2024-06-04 20:29:17 +0100

    This is time i really forgot to commit.

--- src/components/Level/ClickMenu.jsx
+++ src/components/Level/ClickMenu.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Styles from "./ClickMenu.module.css";
+import PropTypes from 'prop-types';
 
-function ClickMenu({ position }) {
+function ClickMenu({ position, setPosition, x, y, setWin }) {
   const { id } = useParams();
   const [characters, setCharacter] = useState([]);
 
@@ -14,26 +15,80 @@ function ClickMenu({ position }) {
 
       const res = await Promise.all([character1, character2, character3]);
 
-      const characters = res.map(c => ({ ...c, key: crypto.randomUUID() }));
+      let key = 0;
+
+      const characters = res.map(c => {
+        const newChar = { ...c, key };
+        key += 1;
+        return newChar;
+      });
 
       setCharacter(characters);
     })();
   }, [id]);
 
+  async function handleClick(character) {
+    const res = await fetch(`https://wheres-who.glitch.me/api/levels/${id}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        character,
+        x,
+        y,
+      })
+    });
+
+    const json = await res.json();
+
+    if (!res.ok) {
+      console.log(json);
+      return;
+    }
+
+    setCharacter(prev => {
+      const newChar = prev.filter(e => e.key !== character);
+
+      if (newChar.length === 0) {
+        setWin(true);
+      }
+
+      return newChar;
+    });
+    setPosition(null);
+    return;
+  }
+
   return (
     position ?
-      <div style={{
-        position: "fixed",
-        left: `${position.left}px`,
-        top: `${position.top}px`
-      }} className={Styles.menu}>
+      <div
+        style={{
+          position: "fixed",
+          left: `${position.left}px`,
+          top: `${position.top}px`
+        }}
+        className={Styles.menu}
+      >
         {characters.map(c => (
-          <img src={c.default} alt="" key={c.key} className={Styles.icon} />
+          <img src={c.default}
+            alt="" key={c.key}
+            className={Styles.icon}
+            onClick={() => handleClick(c.key)}
+          />
         ))}
       </div>
       :
       <></>
   );
 }
+
+ClickMenu.propTypes = {
+  position: PropTypes.object,
+  setPosition: PropTypes.func,
+  x: PropTypes.number,
+  y: PropTypes.number,
+  setWin: PropTypes.func,
+};
 
 export default ClickMenu;
--- src/components/Level/index.jsx
+++ src/components/Level/index.jsx
@@ -3,22 +3,18 @@ import { useParams } from "react-router-dom";
 import NavigationBar from "../NavigationBar";
 import Styles from "./index.module.css";
 import ClickMenu from "./ClickMenu";
-
-function formatTime(seconds, minutes) {
-  let formated = "";
-  minutes <= 9 ? formated = `0${minutes}` : formated = `${minutes}`;
-  seconds <= 9 ? formated = `${formated}:0${seconds}` : formated = `${formated}:${seconds}`;
-
-  return formated;
-}
+import { formatTime } from "../../utiles";
+import { useNavigate } from "react-router-dom";
 
 function Level() {
   const { id } = useParams();
   const [img, setImg] = useState("");
-  const [time, setTime] = useState("00:00");
   const [seconds, setSeconds] = useState(0);
-  const [minutes, setMinutes] = useState(0);
   const [position, setPosition] = useState();
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const [win, setWin] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     (async () => {
@@ -28,23 +24,50 @@ function Level() {
   }, [id]);
 
   useEffect(() => {
-    const timer = setInterval(() => {
-      const formated = formatTime(seconds, minutes);
-      setTime(formated);
+    (async () => {
 
-      if (seconds === 59) {
-        setSeconds(0);
-        setMinutes(prev => prev + 1);
+      if (!win) {
         return;
       }
 
-      setSeconds(prev => prev + 1);
+      const name = prompt("Enter you Name:");
+
+      const res = await fetch(`http://localhost:3000/api/leaderboard/${id}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name,
+          record: seconds,
+        })
+      });
+
+      const json = await res.json();
+
+      if (!res.ok) {
+        console.log(json);
+        return;
+      }
+
+      console.log(json);
+      navigate("/");
+    })();
+  });
+
+  useEffect(() => {
+    if (win) {
+      return;
+    }
+
+    const timer = setInterval(() => {
+      return setSeconds(prev => prev + 1);
     }, 1000);
 
     return () => {
       clearInterval(timer);
     };
-  }, [minutes, seconds, time]);
+  }, [win]);
 
   function handleClick(e) {
     return setPosition(prev => (
@@ -55,13 +78,44 @@ function Level() {
     ));
   }
 
+  function calculatePosition(e) {
+    const rect = e.target.getBoundingClientRect();
+
+    const originalWidth = e.target.naturalWidth;
+    const originalHeight = e.target.naturalHeight;
+
+    const currentWidth = rect.width;
+    const currentHeight = rect.height;
+
+    const scaleX = currentWidth / originalWidth;
+    const scaleY = currentHeight / originalHeight;
+
+    const x = Math.floor((e.clientX - rect.left) / scaleX);
+    const y = Math.floor((e.clientY - rect.top) / scaleY);
+
+    setX(x);
+    setY(y);
+  }
+
   return (
     <>
       <NavigationBar />
-      <ClickMenu position={position} />
-      <h1 className={Styles.time}>{time}</h1>
-      <div className={Styles.container} onClick={handleClick}>
-        <img src={img} alt="" className={Styles.img} />
+      <ClickMenu
+        position={position}
+        setPosition={setPosition}
+        x={x}
+        y={y}
+        setWin={setWin} />
+      <h1 className={Styles.time}>{formatTime(seconds)}</h1>
+      <div className={Styles.container} >
+        <img src={img}
+          alt=""
+          className={Styles.img}
+          onClick={(e) => {
+            handleClick(e);
+            calculatePosition(e);
+          }}
+        />
       </div>
     </>
   );
